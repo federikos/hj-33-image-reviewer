7a533a1a9bfa7addbb9aa034008eb5111e0a7c0d
federikos <federikos@bk.ru>
2019-08-02 03:43:09 +0300

    add right switching of the menu modes

--- js/solution.js
+++ js/solution.js
@@ -2,10 +2,16 @@
 
 const app = document.querySelector('.app');
 const menu = document.querySelector('.menu');
-const currentImage = document.querySelector('.current-image');
 const menuItemNew = document.querySelector('.menu__item.new');
+const menuItemBurger = document.querySelector('.menu__item.burger');
+const menuItemDrag = document.querySelector('.menu__item.drag');
+const menuModeComments = document.querySelector('.mode.comments');
+const menuModeDraw = document.querySelector('.mode.draw');
+const menuModeShare = document.querySelector('.mode.share');
+const currentImage = document.querySelector('.current-image');
 const commentsForm = document.querySelector('.comments__form');
 const imageLoader = document.querySelector('.image-loader');
+const error = document.querySelector('.error');
 let fileInput;
 
 function centerElement(el) {
@@ -14,11 +20,37 @@ function centerElement(el) {
   el.style.setProperty('--menu-left', `${bounds.right / 2 - menu.clientWidth / 2}px`);
 }
 
+function showErr(msg) {
+  const currentErr = error.cloneNode(true);
+
+  if(msg) {
+    const currentMsg = currentErr.querySelector('.error__message');
+    currentMsg.innerText = msg;
+  }
+  error.parentElement.insertBefore(currentErr, error);
+  currentErr.style.display = 'block';
+}
+
+function hideErrors() {
+  const errors = document.querySelectorAll('.error');
+  [...errors].forEach(error => error.style.display = 'none');
+}
+
 function handleFileChange(e) {
-  if (e.target.classList.contains('app') && e.target.dataset.state !== 'initial') {
+  hideErrors();
+
+  if (e.currentTarget.dataset.state && e.currentTarget.dataset.state !== 'initial') {
+    showErr('Чтобы загрузить новое изображение, пожалуйста, воспользуйтесь пунктом "Загрузить новое" в меню.');
     return;
   }
+
   const img = e.dataTransfer ? e.dataTransfer.files[0] : e.currentTarget.files[0];
+  
+  if(!(img.type === "image/jpeg" || img.type === "image/png")) {
+    showErr();
+    return;
+  }
+
   imageLoader.style.display = 'block';
   menu.style.display = 'none';
   
@@ -26,21 +58,35 @@ function handleFileChange(e) {
    .catch(error => console.error('Ошибка:', error))
    .then(res => {
      currentImage.src = res.url;
-     menu.dataset.state = 'default';
+     sessionStorage.setItem('currentImgSrc', res.url);
+     menu.dataset.state = 'selected';
+     menuItemBurger.style.display = 'inline-block';
+     menuModeShare.dataset.state = 'selected';
+     app.dataset.state = 'default';
+     sessionStorage.setItem('state', 'default');
      menu.style.display = 'block';
-     centerElement(menu);
      currentImage.style.display = 'block';
      imageLoader.style.display = 'none';
+     menuItemNew.style.display = 'none';
    });
 }
 
 function init() {
-  menu.dataset.state = 'initial';
-  currentImage.style.display = 'none';
+  menu.dataset.state = sessionStorage.getItem('state') || 'initial';
   commentsForm.style.display = 'none';
+  currentImage.style.display = 'none';
+  menuItemBurger.style.display = 'none';
   centerElement(menu);
+
+  if(sessionStorage.getItem('state')) {
+    currentImage.style.display = 'block';
+  }
+
+  if(sessionStorage.getItem('currentImgSrc')) {
+    currentImage.src = sessionStorage.getItem('currentImgSrc');
+  }
   
-  fileInput = document.createElement('input');
+  fileInput = fileInput || document.createElement('input');
   fileInput.setAttribute('type', 'file');
   fileInput.setAttribute('accept', 'image/jpeg, image/png');
   fileInput.addEventListener('change', handleFileChange);
@@ -50,7 +96,6 @@ function init() {
     e.preventDefault();
     handleFileChange(e);
   });
-  console.log(app);
 }
 
 function publicNewImage(img) {
@@ -65,9 +110,21 @@ function publicNewImage(img) {
   .then(res => res.json())
 }
 
-menuItemNew.addEventListener('click', e => {
-  e.preventDefault();
-  fileInput.click();
+menuItemNew.addEventListener('click', e => fileInput.click());
+
+menuItemBurger.addEventListener('click', e => {
+  e.currentTarget.parentElement.dataset.state = 'default';
+  [menuModeComments, menuModeDraw, menuModeShare].forEach(li => li.dataset.state = '');
+  menuItemNew.style.display = 'inline-block';
 });
+
+[menuModeComments, menuModeDraw, menuModeShare].forEach(li => 
+  li.addEventListener('click', e => {
+    menu.dataset.state = 'selected';
+    e.currentTarget.dataset.state = 'selected';
+    menuItemNew.style.display = 'none';
+    menuItemBurger.style.display = 'inline-block';
+  })
+);
 
 init();
